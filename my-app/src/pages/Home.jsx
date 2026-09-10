35d8e4ed3c94a9411b7e2435b6ba6f65ee926f7b
Warisarapohn <kanchita_w@su.ac.th>
2026-09-10 08:44:23 +0000

    new data and statement

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { movies } from "../data";
 import MovieCard from "../Components/MovieCard";
 import MovieGallery from "../Components/MovieGallery";
